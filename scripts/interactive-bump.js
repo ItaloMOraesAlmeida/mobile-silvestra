bc76b30ea92df3d50d2ec76cc0eded9e44acb5b3
Italo Moraes Almeida <italomoraesalmeida@hotmail.com>
2025-11-26 13:18:53 -0300

    :gift: (feat) Ajuste do scrip de versão

--- scripts/interactive-bump.js
+++ scripts/interactive-bump.js
@@ -29,7 +29,7 @@ process.on("SIGINT", () => {
     )
   );
   console.error(
-    chalk.dim("--------------------------------------------------\n")
+    chalk.dim("---------------------------------------------------\n")
   );
 
   // Sai com código 0: Sucesso para o Git continuar
@@ -78,7 +78,7 @@ const run = async () => {
         chalk.bold.magenta(homologVersion)
     );
     console.log(
-      chalk.dim("--------------------------------------------------")
+      chalk.dim("---------------------------------------------------")
     );
 
     // Pergunta 1: Deseja alterar a versão?
@@ -154,13 +154,13 @@ const run = async () => {
     }
 
     console.log(
-      chalk.dim("\n--------------------------------------------------")
+      chalk.dim("\n---------------------------------------------------")
     );
     console.log(chalk.bold.white("🔍 Resumo das Alterações:"));
     console.log(`   De:   ${chalk.red(currentVersion)}`);
     console.log(`   Para: ${chalk.green(newVersion)}`);
     console.log(
-      chalk.dim("--------------------------------------------------")
+      chalk.dim("---------------------------------------------------")
     );
 
     // Pergunta 3: Confirmação final
